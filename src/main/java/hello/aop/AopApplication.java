476e4f7a2ff13cb4625d9203ef6d9937b2c81025
aammddkkzxc <ideal0427@naver.com>
2024-10-24 03:57:50 +0900

    feat: 스프링 AOP 구현5 - 어드바이스 순서 - 어드바이스 단위가 아니라 클래스 단위로 적용할 수 있다. 하나의 애스펙트에 여러 어드바이스가 있으면 순서를 보장 받을 수 없다. - 따라서 어드바이스들을 별도의 애스펙트 클래스로 분리해야 한다

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -3,13 +3,15 @@ package hello.aop;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV3;
 import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(AspectV1.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 @SpringBootApplication
 public class AopApplication {
 
