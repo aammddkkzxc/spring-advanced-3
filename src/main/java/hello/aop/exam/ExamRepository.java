f070395e36adc6627bdc12075ceda502af80bc6a
aammddkkzxc <ideal0427@naver.com>
2024-10-25 03:24:16 +0900

    feat: 재시도 AOP int value() default 3; 적용할 때 어노테이션에서 값 수정 가능

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,8 @@ public class ExamRepository {
      * 5번에 1번 실패하는 요청
      */
     @Trace
+//    @Retry(value = 4) 값 수정 가능
+    @Retry
     public String save(String itemId) {
         seq++;
 
--- src/main/java/hello/aop/exam/aop/TraceAspect.java
+++ src/main/java/hello/aop/exam/aop/TraceAspect.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
+@Component
 public class TraceAspect {
 
     @Before("@annotation(hello.aop.exam.annotation.Trace)")
